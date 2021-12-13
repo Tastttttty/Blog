d8a5e1530e78df9757989243dad7aac49daca8ca
zhouxin <tastttttty@gmail.com>
2021-12-13 18:59:52 +0800

    第一次推送，包含数据库代码，前端代码，基本springboot的yml配置文件

---
+++ src/test/java/com/tasty/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
